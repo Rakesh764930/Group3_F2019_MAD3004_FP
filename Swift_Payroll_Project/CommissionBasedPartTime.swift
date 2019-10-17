84e712b235d693b2a5c549bae6f73b8535756b3b
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-16 20:01:18 -0400

    CommissionBasedPartTime class created

---
+++ Swift_Payroll_Project/CommissionBasedPartTime.swift
@@ -0,0 +1,12 @@
+//
+//  CommissionBasedPartTime.swift
+//  Swift_Payroll_Project
+//
+//  Created by MacStudent on 2019-10-16.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class CommissionBasedPartTime{
+    
+}
