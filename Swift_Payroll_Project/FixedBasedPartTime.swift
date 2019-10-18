af2d0117d34140f3a57b96072830806d9f98017b
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-17 21:32:21 -0400

    new class created

---
+++ Swift_Payroll_Project/FixedBasedPartTime.swift
@@ -0,0 +1,24 @@
+//
+//  FixedBasedPartTime.swift
+//  Swift_Payroll_Project
+//
+//  Created by MacStudent on 2019-10-17.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class FixedBasedPartTime : PartTime
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
