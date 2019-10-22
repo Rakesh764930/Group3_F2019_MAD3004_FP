c7ff6352416880177d0592ce9e5cf40d9e9815ea
Rakesh Kumar <rakeshkumar92215@gmail.com>
2019-10-22 16:50:28 -0400

    new class intern

---
+++ Swift_Payroll_Project/Intern.swift
@@ -0,0 +1,12 @@
+//
+//  Intern.swift
+//  Swift_Payroll_Project
+//
+//  Created by MacStudent on 2019-10-22.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class intern {
+    
+}
